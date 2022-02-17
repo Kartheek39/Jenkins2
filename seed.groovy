683a43de2e3249288901ae7b51d5376d1e767524
Kartheek39 <Kartheek39>
2022-02-17 19:36:11 +0530

    template

--- seed.groovy
+++ seed.groovy
@@ -27,27 +27,27 @@ folder('CI-Pipelines') {
 def component = ["cart", "catalogue", "user", "payment", "shipping", "frontend"]
 
 for (int i = 0; i < component.size(); i++) {
-  println component[i]
- pipelineJob('CI-Pipelines/${component[i]}') {
-  configure { flowdefinition ->
-    flowdefinition << delegate.'definition'(class:'org.jenkinsci.plugins.workflow.cps.CpsScmFlowDefinition',plugin:'workflow-cps') {
-      'scm'(class:'hudson.plugins.git.GitSCM',plugin:'git') {
-        'userRemoteConfigs' {
-          'hudson.plugins.git.UserRemoteConfig' {
-            'url'('hhttps://github.com/Kartheek39/Jenkins2.git')
-          }
+    println component[i]
+    pipelineJob('CI-Pipelines/${component[i]}') {
+        configure { flowdefinition ->
+            flowdefinition << delegate.'definition'(class: 'org.jenkinsci.plugins.workflow.cps.CpsScmFlowDefinition', plugin: 'workflow-cps') {
+                'scm'(class: 'hudson.plugins.git.GitSCM', plugin: 'git') {
+                    'userRemoteConfigs' {
+                        'hudson.plugins.git.UserRemoteConfig' {
+                            'url'('hhttps://github.com/Kartheek39/Jenkins2.git')
+                        }
+                    }
+                    'branches' {
+                        'hudson.plugins.git.BranchSpec' {
+                            'name'('*/main')
+                        }
+                    }
+                }
+                'scriptPath'('Jenkinsfile')
+                'lightweight'(true)
+            }
         }
-        'branches' {
-          'hudson.plugins.git.BranchSpec' {
-            'name'('*/main')
-          }
-        }
-      }
-      'scriptPath'('Jenkinsfile')
-      'lightweight'(true)
     }
-  }
- }
 }
 
 
